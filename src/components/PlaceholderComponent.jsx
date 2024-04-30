57b404f2b5698db1ed5c0085511b1116ad7a2ab9
Nate Johnson <njohnson2897@gmail.com>
2024-04-30 14:49:11 -0500

    adds .eslintrc file

--- src/components/PlaceholderComponent.jsx
+++ src/components/PlaceholderComponent.jsx
@@ -1,7 +1,9 @@
-export const PlaceholderComponent =  ()  =>  {
+function PlaceholderComponent() {
     return (
     <div>
         <h1>This is a placeholder</h1>;
     </div>
     );
 }
+
+export default PlaceholderComponent;
