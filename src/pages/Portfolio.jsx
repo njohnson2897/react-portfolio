2d8b5384f846fcb646e4ed665ddefefa16393f6e
Nate Johnson <njohnson2897@gmail.com>
2024-05-01 15:17:32 -0500

    begins to set up logic for rendering portfolio projects and imports screenshots

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,21 @@
 export default function Portfolio() {
     return (
-        <div>My Projects:</div>
+        <div>
+            <h1>My Projects:</h1>
+            <ul className="list-group">
+            {props.projects.map((project) => (
+        <li className="list-group-item" key={project.id}>
+          <img
+            alt={project.title}
+            className="img-fluid"
+            src={project.images.original.url}
+          />
+        </li>
+      ))}
+    </ul>
+            
+            
+        
+        </div>
     )
 }
---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export default myProjectData = [
+    {
+        name: "trivl",
+        url: "https://njohnson2897.github.io/trivl-project/",
+        githubUrl: "https://github.com/njohnson2897/trivl-project",
+        imgSrc: "../assets/trivl.jpg"
+    },
+    {
+        name: "activ",
+        url: "https://activ.onrender.com/",
+        githubUrl: "https://github.com/bmallar/Activ",
+        imgSrc: "../assets/activ.jpg"
+    },
+    {
+        name: "weatherApp",
+        url: "https://njohnson2897.github.io/my-weather-dashboard/",
+        githubUrl: "https://github.com/njohnson2897/my-weather-dashboard",
+        imgSrc: "../assets/weatherApp.jpg"
+    },
+    {
+        name: "projectTracker",
+        url: "https://njohnson2897.github.io/my-project-tracker/",
+        githubUrl: "https://github.com/njohnson2897/my-project-tracker",
+        imgSrc: "../assets/projectTracker.jpg"
+    },
+    {
+        name: "JATE",
+        url: "https://text-editor-project.onrender.com/",
+        githubUrl: "https://github.com/njohnson2897/text-editor-project",
+        imgSrc: "../assets/jate.jpg"
+    },
+    {
+        name: "noteTaker",
+        url: "https://my-notes-project.onrender.com/",
+        githubUrl: "https://github.com/njohnson2897/my-notes-project",
+        imgSrc: "../assets/noteTaker.jpg"
+    },
+];
