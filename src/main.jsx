0ef7ff158bb2f3ae94f5be3aa2543df2f97ef218
Nate Johnson <njohnson2897@gmail.com>
2024-05-01 08:52:51 -0500

    imports all necessary pages  to main.jsx  and sets up router

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,40 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import AboutPage from './pages/AboutPage';
+import ErrorPage from './pages/ErrorPage'
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <AboutPage />,
+        },
+        {
+          path: 'contact',
+          element: <Contact />,
+        },
+        {
+          path: 'portfolio',
+          element: <Portfolio />,
+        },
+        {
+            path: 'resume',
+            element: <Resume />,
+        },
+      ],
+    },
+  ]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}  />);
