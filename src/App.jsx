bde08878eb6639f5fadef278528cefc3f55ef191
Nate Johnson <njohnson2897@gmail.com>
2024-05-01 08:56:22 -0500

    populates each page so that the webpage renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from "./components/Header.jsx";
-import Nav from "./components/Navigation.jsx";
 import Footer from "./components/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
