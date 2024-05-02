fb1e819cae342f7bb9786cd9f292a76cf1e91bef
Nate Johnson <njohnson2897@gmail.com>
2024-05-01 19:04:50 -0500

    turns footer  icons into links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { MdEmail } from "react-icons/md";
 export default function Footer() {
     return (
         <div className='bg-secondary text-center fs-3'>
-        <FaGithub className='mx-3'/>
-        <FaLinkedin className='mx-3' />
-        <MdEmail  className='mx-3' />
+        <a href='https://github.com/njohnson2897'><FaGithub className='mx-3 text-white'/></a>
+        <a href='https://linkedin.com/in/nate-johnson-87396422b'><FaLinkedin className='mx-3  text-white' /></a>
+        <a href='mailto:njohnson2897@gmail.com'><MdEmail  className='mx-3 text-white' /></a>
         </div>
     );
 }
